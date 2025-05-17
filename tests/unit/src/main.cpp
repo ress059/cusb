67dfcf7b5606e846e389d65647d7bc9661ed8ea9
Ian Ress <ir232@cornell.edu>
2025-05-17 10:09:26 -0400

    Add unit test build configuration.

---
+++ tests/unit/src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief Run all unit tests.
+ * 
+ * @author Ian Ress
+ * @version 0.1
+ * @date 2024-03-11
+ * @copyright Copyright (c) 2024
+ */
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int ac, char** av)
+{
+    return RUN_ALL_TESTS(ac, av);
+}
